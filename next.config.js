792efb5a132c49492b8aa0531fa2705bc259e9d3
Karen <karen.borrero@grupodinosol.es>
2024-07-04 12:24:11 +0100

    Service ✨ new service reactive

--- next.config.js
+++ next.config.js
@@ -3,38 +3,49 @@
  */
 
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    typedRoutes: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "a0.muscache.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.gstatic.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+    reactStrictMode: false,
+    experimental: {
+        typedRoutes: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'a0.muscache.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.gstatic.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
 };
 
 module.exports = nextConfig;
