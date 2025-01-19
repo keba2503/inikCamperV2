22d9b9445d918d0335ae4bc6b4442fc02d755e5d
Karen <karen.borrero@grupodinosol.es>
2025-01-19 19:56:01 +0000

    fix 🐛 languaje default

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,23 +1,19 @@
 'use client';
 
-import React, { createContext, useState, ReactNode, useEffect } from 'react';
+import React, {createContext, useState, ReactNode, useEffect} from 'react';
 
 interface LanguageContextType {
     language: string;
     switchLanguage: (newLanguage: string) => void;
 }
 
-export const LanguageContext = createContext<LanguageContextType | undefined>(
-    undefined,
-);
+export const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
-export const LanguageProvider: React.FC<{ children: ReactNode }> = ({
-                                                                        children,
-                                                                    }) => {
-    const [language, setLanguage] = useState<string>('en');
+export const LanguageProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+    const [language, setLanguage] = useState<string>('es');
 
     useEffect(() => {
-        const savedLanguage = localStorage.getItem('language') || 'en';
+        const savedLanguage = localStorage.getItem('language') || 'es';
         setLanguage(savedLanguage);
     }, []);
 
@@ -27,7 +23,7 @@ export const LanguageProvider: React.FC<{ children: ReactNode }> = ({
     };
 
     return (
-        <LanguageContext.Provider value={{ language, switchLanguage }}>
+        <LanguageContext.Provider value={{language, switchLanguage}}>
             {children}
         </LanguageContext.Provider>
     );
